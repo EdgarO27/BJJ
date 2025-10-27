80baea01c69ccf08ee5527e02d78d7d18599a16f
edgaro9 <edgaro@cpp.edu>
2025-10-27 01:38:00 -0700

    Set up

---
+++ test_img.py
@@ -0,0 +1,19 @@
+import cv2
+import os
+print(cv2.__version__)
+
+
+
+# vid = cv2.VideoCapture("C:\\Users\\Admin\\PycharmProjects\\project_1\\openCV.mp4")
+
+# count, success = 0, True
+# while success:
+#     success, image = vid.read() # Read frame
+#     if success: 
+#         cv2.imwrite(f"frame{count}.jpg", image) # Save frame
+#         count += 1
+
+# vid.release()
+
+
+# https://www.youtube.com/watch?v=ET3XrD-R574
