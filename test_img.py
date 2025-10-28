66482de25912ff45a96bbcc903508856cc360de3
edgaro9 <edgaro@cpp.edu>
2025-10-27 18:19:08 -0700

    Adding video to capture it by frames

--- test_img.py
+++ test_img.py
@@ -1,8 +1,25 @@
-import cv2
-import os
-print(cv2.__version__)
+import numpy as np
+import cv2 as cv
+count = 0
+cap = cv.VideoCapture('C:\Projects\AI\Image_class_bjj\Armbar by Estevan Martinez [ET3XrD-R574].f299.mp4') 
+#  grabbing the video we want no sound since we are capturing body mechanics
+while cap.isOpened():
+    ret, frame = cap.read()
+    
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
+    cv.imwrite("frame%d.jpg" % count, frame)
+    count = count + 1
+    cv.imshow('frame', gray)
+    if cv.waitKey(1) == ord('q'):
+        break
 
+cap.release()
+cv.destroyAllWindows()
 
 # vid = cv2.VideoCapture("C:\\Users\\Admin\\PycharmProjects\\project_1\\openCV.mp4")
 
@@ -16,4 +33,4 @@ print(cv2.__version__)
 # vid.release()
 
 
-# https://www.youtube.com/watch?v=ET3XrD-R574
+# yt-dlp -f "bv*[ext=.mp4]+ba[ext=.m4a]/b[ext=.mp4] / bv*+ba/b" "https://www.youtube.com/watch?v=ET3XrD-R574"
